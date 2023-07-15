2745c93b3a6beb8647a753df66b4027bbebd3cee
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-07-15 14:55:24 +0500

    draw bricks

--- game.py
+++ game.py
@@ -9,15 +9,31 @@ running = True
 dt = 0
 
 TEAL = (0, 128, 128)
+ORANGE = (255, 165, 0)
 BLACK = (0, 0, 0)
 
-# Set up the player
+# set up the player
 player_color = TEAL
 player_width = 100
 player_height = 10
 player_x = (width - player_width) // 2
 player_y = screen.get_height() / 1.2
 player_speed = 2
+
+# set up bricks
+brick_color = ORANGE
+brick_width = 100
+brick_height = 30
+brick_rows = 5
+brick_cols = width // (brick_width + 5)
+padding = 5
+bricks = []  # contains the undestroyed bricks
+
+for row in range(brick_rows):
+    for col in range(brick_cols):
+        brick_x = col * (brick_width + padding) + 10
+        brick_y = row * (brick_height + padding) + 50
+        bricks.append(pygame.Rect(brick_x, brick_y, brick_width, brick_height))
 
 # game loop
 while running:
@@ -32,6 +48,11 @@ while running:
     # draw player (paddle)
     pygame.draw.rect(screen, TEAL, (player_x, player_y,
                      player_width, player_height))
+
+    # draw bricks
+    for brick in bricks:
+        pygame.draw.rect(screen, brick_color, brick)
+        pygame.draw.rect(screen, BLACK, brick, 1)  # brick outlines for padding
 
     # move player
     keys = pygame.key.get_pressed()
